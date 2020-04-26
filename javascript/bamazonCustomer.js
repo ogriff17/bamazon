e3a4fa48405fe3f9b60a141118a3b155841288c8
Cerridwen <ogriff17@gmail.com>
2020-04-26 18:53:52 -0400

    adding changes to the js file and making sure the sql ius working

--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -1,14 +1,69 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var colors = require("colors");
-var table = require("cli-table");
+var Table = require("cli-table");
 
-console.log("haha it works");
+
 
 var connection = mysql.createConnection(
     { host: "localhost",
-    port:3306;
+    port:3306,
     user:"root",
     password:"",
     database: "bamazon",
+}); 
+
+connection.connect(function(err)
+{  
+    if (err) throw err;
+    console.log(colors.cyan("Welcome! You are now connected to the store, peasant."));
+    shopping();
+
 });
+
+function shopping(){
+    console.log("We're shopping now");
+    connection.query('SELECT * FROM products', function (err, res){
+        if (err) throw err;
+        var table = new Table (
+         {
+            head: ["Product_name".cyan.bold, "Item_id".cyan.bold, "Department_name".cyan.bold, "Price".cyan.bold, "Stock_quantity".cyan.bold],
+            colWidths: [12, 12, 12, 12, 12],
+           
+         });
+         
+         for (i=0; i < res.length; i++){
+             table.push([res[i].Product_name, res[i].Item_id, res[i].Department_name, res[i].Price, res[i].Stock_quantity]);
+         }
+         console.log(table);
+ 
+        
+
+    
+
+         
+         console.log("res =" + res); 
+        
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log("haha it works");
+
